d5b039d8c829a44575c1ef860e5fd1a2742c8ce8
Ada <github@adapdr.me>
2025-02-21 17:32:46 +0100

    fix(Backend): :bug: Added postgres UUID interpretation globally

--- src/acme/main.py
+++ src/acme/main.py
@@ -4,10 +4,14 @@ Main Application file
 - Collects routers and mounts/includes them to the API
 """
 
+import psycopg2.extras
 from api.routers import routers
 from config import Config
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+
+# ? Required so postgres understands <UUID>
+psycopg2.extras.register_uuid()
 
 # ? Initialize application with configs
 app = FastAPI(**Config.API.model_dump())
