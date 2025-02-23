e9013728a2b001f09eddd4a4f9c7d1a70ad24ab3
Ada <github@adapdr.me>
2025-02-23 17:37:24 +0100

    chore: :wastebasket: Removed unused files

--- magic.sh
+++
@@ -1,5 +0,0 @@
-
-
-python manage.py migrate
-# python manage.py createsuperuser --noinput --username admin --email admin@example.com
-python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')"
